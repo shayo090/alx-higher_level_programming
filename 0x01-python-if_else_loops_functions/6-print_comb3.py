25a02d76fb0ae24bd7c175974e89925f2128fbd1
Shayo090 <abdulsayoboy@gmail.com>
2022-08-23 08:55:12 -0700

    tasks

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in  range (0, 9):
-    for j in range (i+1, 10):
+for i in  range(0, 9):
+    for j in range(i+1, 10):
         if int(str(i) + str(j)) < 89:
             print("{}{}".format(i, j), end=", ")
         else:
