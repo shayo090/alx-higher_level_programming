863c03a32b11389afe59358239ab0c8b87d60892
Shayo090 <abdulsayoboy@gmail.com>
2022-08-25 12:49:45 -0700

    my calculator redo

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,22 +1,39 @@
 #!/usr/bin/python3
 from sys import argv
 from calculator_1 import add, sub, mul, div
-if __name__ != "__main__":
+
+if __name__ == '__main__':
 
     argc = len(argv) - 1
-    if argc != 3:
-        print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
-        exit(1)
-    elif argv[2] == '+':
-        result = add(int(argv[1]), int(argv[3]))
-    elif argv[2] == '-':
-        result = sub(int(argv[1]), int(argv[3]))
-    elif argv[2] == '*':
-        result = mul(int(argv[1]), int(argv[3]))
-    elif argv[2] == '/':
-        result = div(int(argv[1]), int(argv[3]))
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
 
-    print("{:d} {:d} {:d} = {:d}".format(argv[1], argv[2], argv[3], result))
+    if argc == 3:
+        operator = argv[2]
+        a = int(argv[1])
+        b = int(argv[3])
+
+        if operator == '+':
+            result = add(a, b)
+            print('{:d} + {:d} = {:d}'.format(a, b, result))
+            exit(0)
+
+        elif operator == '-':
+            result = sub(a, b)
+            print('{:d} - {:d} = {:d}'.format(a, b, result))
+            exit(0)
+
+        elif operator == '*':
+            result = mul(a, b)
+            print('{:d} * {:d} = {:d}'.format(a, b, result))
+            exit(0)
+
+        elif operator == '/':
+            res = div(a, b)
+            print('{:d} / {:d} = {:d}'.format(a, b, result))
+            exit(0)
+
+        else:
+            print('Unknown operator. Available operators: +, -, * and /')
+            exit(1)
+    else:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        exit(1)
