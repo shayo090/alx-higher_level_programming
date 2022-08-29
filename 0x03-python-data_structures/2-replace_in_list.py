ddb8ab8e6dff55e462e145930a156ec301dbf016
Shayo090 <abdulsayoboy@gmail.com>
2022-08-29 05:47:20 -0700

    data structure

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,5 +2,6 @@
 
 def replace_in_list(my_list, idx, element):
     if idx >= len(my_list) or idx < 0:
-        mu_list[idx] = element
-        return (my_list)
+        return
+    mu_list[idx] = element
+    return (my_list)
