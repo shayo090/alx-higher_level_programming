151d00fbf230e48571fbf0093633cea9919fe922
Shayo090 <abdulsayoboy@gmail.com>
2022-08-29 06:28:22 -0700

    data structure

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    for row in marix:
+        for col in range(len(row)):
+            if col == len(row) - 1:
+                print("{:d}".format(row[col]), end="")
+            else:
+                print("{:d}".format(row[col]), end=" ")
