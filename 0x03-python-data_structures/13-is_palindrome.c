d21e700aec86119e8735b1a1276ba07077fd682d
Shayo090 <abdulsayoboy@gmail.com>
2022-08-29 14:28:19 -0700

    last

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,40 +1,94 @@
 #include "lists.h"
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
-  * is_palindrome - A function that checks if a list is a palindrome.
-  * @head: The pointer to the head of the list.
-  * Return: 0 if list not a palindrome, 1 if it is.
-  */
+ * reserve_listint - reverses a linked list
+ *
+ * @head: A pointer to the first element in the list
+ *
+ * Return: A pointer to the beginning of the list
+ *
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *transv, *nextnode;
+
+	transv = *head;
+	nextnode = *head;
+	*head = NULL;
+
+	while (nextnode != NULL)
+	{
+		nextnode = nextnode->next;
+		transv->next = *head;
+		*head = transv;
+		transv = nextnode;
+	}
+
+	return (*head);
+}
+
+/**
+ * listint_len - returns the number of element in a loinked list
+ *
+ * @head: pointer to the first element of the list
+ *
+ * Return: Number of element in the list
+ */
+
+size_t listint_len(listint_t *head)
+{
+	size_t no_nodes;
+	listint_t *transv;
+
+	transv = head;
+	no_nodes = 0;
+	while (transv != NULL)
+	{
+		no_nodes++;
+		transv = transv->next;
+	}
+	return (no_nodes);
+}
+
+/**
+ * is_palindrome - compared the linked list with its reversed if their equal
+ *
+ * @head: pointer to the first element in the list
+ *
+ * Return: 0 if it is not a palindrome
+ * Otherwise: 1 if it is a palindrome
+ */
+
 int is_palindrome(listint_t **head)
 {
-	listint_t *tmp = *head;
-	int nodes = 0, i = 0, *array = NULL;
+	listint_t *rev_list, *transv, *mid_list;
+	size_t nodes, i;
 
-	if (*head == NULL || head == NULL || (*head)->next == NULL)
-		return (1);
-	while (tmp)
+	if (*head == NULL || (*head)->next == NULL)
+		return (1); /*An Empty list or one with a single node */
+	nodes = listint_len(*head); /* get the size of list */
+
+	transv = *head;
+	/* Get the middle of the list */
+	for (i = 0; i < (nodes / 2) - 1; i++)
+		transv = transv->next;
+	if ((nodes % 2) == 0 && transv->n != transv->next->n)
+		return (0);
+	transv = transv->next->next;
+	rev_list = reverse_listint(&transv);
+	mid_list = rev_list;
+
+	transv = *head;
+	while (rev_list != NULL)
 	{
-		nodes++;
-		tmp = tmp->next;
-	}
-	array = malloc(sizeof(int) * nodes);
-	tmp = *head;
-	while (tmp)
-	{
-		array[i++] = tmp->n;
-		tmp = tmp->next;
-	}
-	for (i = 0; i < nodes / 2; i++)
-	{
-		if (array[i] != array[nodes - 1 - i])
-		{
-			free(array);
+		if (transv->n != rev_list->n)
 			return (0);
-		}
+		transv = transv->next;
+		rev_list = rev_list->next;
 	}
-	free(array);
+	reverse_listint(&mid_list);
 	return (1);
 }
