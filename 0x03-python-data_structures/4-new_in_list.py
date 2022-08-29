3142bc7e54afbee7d3ef1a2a114f4cec0ad6d228
Shayo090 <abdulsayoboy@gmail.com>
2022-08-29 06:05:55 -0700

    data structure

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    if idx >= len(my_list) od idx < 0:
+        return(my_list)
+    new_list = mylist.copy()
+    new_list[idx] = element
+    return(new_list)
