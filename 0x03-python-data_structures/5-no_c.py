5739e51ba8d0e5745ac3b6a9e21f87489d128eca
Shayo090 <abdulsayoboy@gmail.com>
2022-08-29 06:20:08 -0700

    data structure

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
+    new_s = ""
     for i in my_string:
-        if my_string[i] != 'cC':
-    return my_string
+        if i not in 'cC':
+            new_s = new_s + i
+    return (new_s)
